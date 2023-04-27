bbd288b6aad67fc99856697f885a5f9591e04f14
nora-sch <nora.schumann.development@gmail.com>
2023-04-27 17:01:43 +0200

    add filter section in html and css

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
-const data = require('../sources/data.js');
-const {getTop10, getAllNoteDesc, getAllNoteAsc} = require('./filters.js');
+// const data = require('../sources/data.js');
+// const {getTop10, getAllNoteDesc, getAllNoteAsc} = require('./filters.js');
 
+// const restaurantTop10 = getTop10(data); // restaurant Top 10
+// const top10Length = getTop10(data).length; // nb of restaurants
+// const allSortDescendant = getAllNoteDesc(data); // all restaurants sorted by note Desc
+// const allSortAscendant = getAllNoteAsc(data); // all restaurants sorted by note Asc
 
-const restaurantTop10 = getTop10(data); // restaurant Top 10
-const top10Length = getTop10(data).length; // nb of restaurants
-const allSortDescendant = getAllNoteDesc(data); // all restaurants sorted by note Desc
-const allSortAscendant = getAllNoteAsc(data); // all restaurants sorted by note Asc
-// console.log(restaurantTop10); 
-// console.log(top10Length); 
-// console.log(allSortDescendant);
-// console.log(allSortAscendant);
+// ===== DOM =====
+
+let slider = document.querySelector(".slider");
+let sliderValue = document.querySelector(".slider").value;
+let sliderValueDisplay = document.querySelector("#slider-value");
+slider.addEventListener("change", (event) => {
+  sliderValueDisplay.innerHTML = sliderValue;
+  console.log(sliderValue);
+  console.log(event);
+});
